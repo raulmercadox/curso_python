8344296fdf1b84f262814d307bd074c1413465c1
RAUL MERCADO GARCIA <raul.mercado.garcia@ericsson.com>
2019-09-15 23:12:04 -0500

    Problem Set 3

---
+++ testGlobalVariable.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 15 13:57:11 2019
+
+@author: ezmerra
+"""
+
+availableGuesses = 8
+
+def showGuesses():
+    print(availableGuesses)
+
+showGuesses()
